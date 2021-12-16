61e6a4861ddbd3fb5e6d0c56892b84d269669530
137xiaoyu <wcx90@outlook.com>
2021-12-16 16:28:26 +0800

    added json files for huge_img_test

--- my_tools/huge_img_test.sh
+++ my_tools/huge_img_test.sh
@@ -1,7 +1,7 @@
-CUDA_VISIBLE_DEVICES=5 python my_tools/huge_img_test.py \
+CUDA_VISIBLE_DEVICES=4 python my_tools/huge_img_test.py \
 /home/wucx/dataset/tzb/input_path/val/img/ \
 work_dirs_tzb/faster_rcnn_orpn_r50_fpn_3x_dota10_mss_tzb_001/img_results/ \
 work_dirs_tzb/faster_rcnn_orpn_r50_fpn_3x_dota10_mss_tzb_001/faster_rcnn_orpn_r50_fpn_3x_dota10_mss_tzb.py \
 work_dirs_tzb/faster_rcnn_orpn_r50_fpn_3x_dota10_mss_tzb_001/epoch_36.pth \
-my_tools/768_train.json \
+my_tools/1024_test_sss.json \
 --method pyplot
